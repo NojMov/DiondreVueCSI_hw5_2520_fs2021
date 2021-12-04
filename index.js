ddf6160d5553cfd744dd6657b71120a32a3ddf41
Diondre Vue <dvue@oakland.edu>
2021-12-04 10:28:24 -0500

    fixed the if statements and vals in app.post

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { render } = require('ejs');
 const express = require('express');
 
 const app = new express();
@@ -37,48 +38,64 @@ app.get('/trivia', (req, res) => {
 
 app.post('/idek', (req, res) => {
     
-}
-
-app.post('/idek', (req, res) => {
-    
     // let ans1 = document.getElementById("a1").value;
     // let ans2 = document.getElementById("a2").value;
     // let ans3 = document.getElementById("a3").value;
     // let ans4 = document.getElementById("a4").value;
     // let ans5 = document.getElementById("a5").value;
     // let ans6 = document.getElementById("a6").value;
-    let ans1 = { answer: req.body.a1}
-    let ans2 = { answer: req.body.a2}
-    let ans3 = { answer: req.body.a3}
-    let ans4 = { answer: req.body.a4}
-    let ans5 = { answer: req.body.a5}
-    let ans6 = { answer: req.body.a6}
+    let ans1 = req.body.a1
+    let ans2 = req.body.a2
+    let ans3 = req.body.a3
+    let ans4 = req.body.a4
+    let ans5 = req.body.a5
 
-    if(ans1 = 3){
+    if(ans1 == "3" || ans1 == "c") {
         i++;
+        console.log("right" + i)
+        console.log(ans1)
+    }else {
+        console.log("wrong")
+        console.log(ans1)
     }
 
-    if(ans2 = 3){
+    if(ans2 == "3" || ans2 == "b"){
         i++;
-    }
+        console.log("right")
+        console.log(ans2)
+    }else{
+        console.log("wrong")
+        console.log(ans2)
+    };
 
-    if(ans3 = 2){
+    if(ans3 == "2" || ans3 == "c"){
         i++;
-    }
+        console.log("right")
+        console.log(ans3)
+    }else{
+        console.log("wrong")
+        console.log(ans3)
+    };
 
-    if(ans4 = 1){
+    if(ans4 == "1" || ans4 == "a"){
         i++;
-    }
+        console.log("right")
+        console.log(ans4)
+    }else{
+        console.log("wrong")
+        console.log(ans4)
+    };
 
-    if(ans5 = 15){
+    if(ans5 == "15" || ans5 == "b"){
         i++;
-    }
+        console.log("right")
+        console.log(ans5)
+    }else{
+        console.log("wrong")
+        console.log(ans5)
+    };
 
-    if(ans6.toLowercase == "yes"){
-        i++;
-    }
-
-    res.send(`you got ` + i + ` out of 6 questions right. tahnk you for playing`)
+    res.send(`you got ` + i + ` out of 5 questions right. tahnk you for playing`)
 });
 
 //start server
